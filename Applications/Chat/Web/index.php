ae90e59f671c631e2c62a016bfe7eb29c0e867ec
jShi-git <jshilib@163.com>
2015-03-27 17:07:34 +0800

    添加运行截图

--- Applications/Chat/Web/index.php
+++ Applications/Chat/Web/index.php
@@ -21,7 +21,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=0">
-  <title>湖南卫视</title>
+  <title>聊天室</title>
   
   <link rel="stylesheet" media="all" href="css/style.css?v=2" />
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chat
-手机网页时时聊天
+手机网页实时聊天
+
+#截图
+<img src="screenshot/1.jpg" width="320" alt="" />
+
+<img src="screenshot/2.jpg" width="320" alt="" />
